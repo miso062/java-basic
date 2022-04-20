1fdd0aed51d81410647f1f8f9d10788a74054c08
bitcamp <bitcamp@DESKTOP-IIFVETB>
2022-04-20 14:27:48 +0900

    4/20 - HW01 calculate grade by using multi jagged array

--- 06_array/src/array_2/Grade2.java
+++ 06_array/src/array_2/Grade2.java
@@ -8,52 +8,60 @@ public class Grade2 {
 		int num;
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("인원수: ");
+		System.out.print("인원수: ");
 		num = scan.nextInt();
 		
 		String[] name = new String[num];
 		String[][] subject = new String[num][];
-		int[][] score = new int[num][];
-		int[] subject_num = new int[num];
 		
-//		int[][] ar = new int[3][];
-//		ar[0] = new int[2];
-//		ar[1] = new int[3];
-//		ar[2] = new int[4];
+		double[] avg = new double[num];
+		int[] subject_num = new int[num];
+		int[] sum = new int[num];
+		
+		int[][] score = new int[num][];
 		
 		
 		for(int i=0; i<num; i++) {
+			System.out.println();
 			
-			System.out.println("이름입력: ");
+			System.out.print("이름입력: ");
 			name[i] = scan.next();
 			
-			System.out.println("과목수 입력: ");
+			System.out.print("과목수 입력: ");
 			subject_num[i] =scan.nextInt(); 
 			
 			subject[i] = new String[subject_num[i]];
+			score[i] = new int[subject_num[i]];
 			
 			for(int j=0; j<subject_num[i]; j++) {
 				System.out.print("과목명 입력: ");
 				subject[i][j] = scan.next();
 			}
 			for(int j=0; j<subject_num[i]; j++) {
-				System.out.print(subject[j] + "점수 입력: ");
+				System.out.print(subject[i][j] + "점수 입력: ");
 				score[i][j] = scan.nextInt();
+				sum[i] += score[i][j]; 
 			}
+			avg[i] = (double)sum[i]/subject_num[i];
 		}
 		
-//		System.out.println(" ___________________________________________________________ ");
-//		System.out.println("|                                                            |");
-//		System.out.println("| 이름\t\t국어\t영어\t수학\t총점\t 평균\t학점 |");
-//		System.out.println("|------------------------------------------------------------|");
-//		for(int i=0; i<num; i++) {
-//			System.out.print("| "+ name[i] + "\t");
-//			for(int j=0; j<subject_num; j++) {
-//				System.out.printf("%3d\t", score[i][j]);
-//			}
-//			System.out.println(String.format("%.2f", avg[i]) + "\t" + String.format("%3c", grade[i]) + "  |");
-//		}
-//		System.out.println("|____________________________________________________________|");
+		System.out.println();
+		
+		for(int i=0; i<num; i++) {
+			System.out.println("========================================================");
+			System.out.print("이름\t");
+			for(int j=0; j<subject_num[i]; j++) {
+				System.out.print(subject[i][j] + "\t");
+			}
+			System.out.println("총점\t평균");
+			System.out.println("--------------------------------------------------------");
+			System.out.print(name[i] + "\t");
+			for(int j=0; j<subject_num[i]; j++) {
+				System.out.print(score[i][j] + "\t");
+			}
+			System.out.println(sum[i] + "\t" + String.format("%.2f", avg[i]));
+		}
+		System.out.println("========================================================");
 		
 		scan.close();
 		
