cd026e69569351c36353f8963cc4ec22b36c93b4
bitcamp <bitcamp@DESKTOP-IIFVETB>
2022-04-27 11:25:50 +0900

    static example

---
+++ 07_class/src/_constructor/ImportStatic.java
@@ -0,0 +1,11 @@
+package _constructor;
+
+import static java.lang.Math.*;
+import static java.lang.System.*;
+
+public class ImportStatic {
+	public static void main(String[] args) {
+		out.println("난수 = " + /*java.lang.Math.*/random());
+		out.println("제곱 = " + pow(2, 5));
+	}
+}
