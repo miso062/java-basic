9c28f29561c2480cd204f353bf75b3b06c92c16e
bitcamp <bitcamp@DESKTOP-IIFVETB>
2022-04-27 10:47:59 +0900

    static example

--- 07_class/src/_class/StaticMain.java
+++ 07_class/src/_class/StaticMain.java
@@ -2,7 +2,19 @@ package _class;
 
 class StaticTest{
 	private int a;
-	private static int b;
+	// 생성자를 통한 초기화가 불가능
+	static int b;
+	
+//	Constructor - 객체의 초기화를 담당
+	public StaticTest(){
+		System.out.println("StaticTest의 기본생성자");
+		
+	}
+	
+	static {
+		System.out.println("Static 초기화 영역");
+		b=3;
+	}
 	
 	public void calc() {
 		a++;
@@ -12,24 +24,31 @@ class StaticTest{
 	public void disp() {
 		System.out.println("a = " + a + " | b = " + b);
 	}
+	
+	public static void output() {
+		System.out.println("static method...");
+		System.out.println(b);
+//		System.out.println("a = " + a + " | b = " + b);
+	}
 }
 
 public class StaticMain {
 	public static void main(String[] args) {
-		StaticTest st1 = new StaticTest();
-		st1.calc();
-		st1.disp();
-		System.out.println();
-		
-		StaticTest st2 = new StaticTest();
-		st2.calc();
-		st2.disp();
-		System.out.println();
-		
-		StaticTest st3 = new StaticTest();
-		st3.calc();
-		st3.disp();
-		System.out.println();
+		System.out.println(StaticTest.b);
+//		StaticTest st1 = new StaticTest();
+//		st1.calc();
+//		st1.disp();
+//		System.out.println();
+//		
+//		StaticTest st2 = new StaticTest();
+//		st2.calc();
+//		st2.disp();
+//		System.out.println();
+//		
+//		StaticTest st3 = new StaticTest();
+//		st3.calc();
+//		st3.disp();
+//		System.out.println();
 		
 //		a는 고정값 | b는 계속 증가
 	}
